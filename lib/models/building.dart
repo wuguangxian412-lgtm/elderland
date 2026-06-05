6a6046b2f180b8b0060e352fb2237a2f3499ff8e
wuguangxian412-lgtm <wuguangxian412@gmail.com>
2026-06-06 01:38:09 +0800

    Add Building model

---
+++ lib/models/building.dart
@@ -0,0 +1,38 @@
+/// 建筑数据模型
+class Building {
+  final String id;
+  final String locationId;
+  final String name;
+  final String type;
+  final String description;
+  final bool isPublic;
+
+  const Building({
+    this.id = '',
+    this.locationId = '',
+    this.name = '',
+    this.type = '',
+    this.description = '',
+    this.isPublic = true,
+  });
+
+  factory Building.fromJson(Map<String, dynamic> json) {
+    return Building(
+      id: (json['id'] as String?)?.trim() ?? '',
+      locationId: (json['locationId'] as String?)?.trim() ?? '',
+      name: (json['name'] as String?)?.trim() ?? '',
+      type: (json['type'] as String?)?.trim() ?? '',
+      description: (json['description'] as String?)?.trim() ?? '',
+      isPublic: json['isPublic'] is bool ? json['isPublic'] as bool : true,
+    );
+  }
+
+  Map<String, dynamic> toJson() => {
+    'id': id,
+    'locationId': locationId,
+    'name': name,
+    'type': type,
+    'description': description,
+    'isPublic': isPublic,
+  };
+}
